32569f6075340d7f7cf659db485ab043474b250e
Andrew Stepura <andriistepura@gmail.com>
2025-10-24 08:34:53 +0100

    get newgrid endpoint draft

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"Hello": "I'm sudoku back-end with python FastAPI"}
+
+@app.get("/newgrid")
+def root():
+    return {
+            "grid": [
+                     {"a1":"TBD grid cell #1"},
+                     {"a2":"TBD grid cell #2"},
+                     {"a3":"TBD grid cell #3"}
+                    ],
+            "error": None
+        }
