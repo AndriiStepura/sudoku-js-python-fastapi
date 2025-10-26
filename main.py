be319f8255b48c7fb39dd27b8c0517367a8b3998
Andrew Stepura <andriistepura@gmail.com>
2025-10-26 23:33:55 +0000

    add second mock and error 404

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from board_mocks import mock1
-from fastapi import FastAPI
+from board_mocks import mock1, mock2
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -13,11 +13,33 @@ def root():
 
 # Endpoint to get new game board
 @app.get("/newgrid")
-def root():
-    return {
-            "gridId": "mock1",
-            "grid": [
-                     mock1
-                    ],
-            "errors": None
-        }
+def new_grid(gridId:str="mock1"):
+    gridInResponse = []
+    responseErrors = []
+
+    mocks = {
+        "mock1": mock1,
+        "mock2": mock2
+    }
+
+    gridInResponse = mocks.get(gridId)
+    if not gridInResponse:
+        responseErrors.append(f"gridId {gridId} NOT FOUND")
+
+    if len(responseErrors) == 0:
+        return {
+                "gridId": gridId,
+                "grid": [
+                            gridInResponse
+                        ],
+                "errors": responseErrors
+            }
+    else:
+        # https://fastapi.tiangolo.com/tutorial/handling-errors/#use-httpexception
+        raise HTTPException(status_code=404, detail=responseErrors)
+    
+
+#TODO add Int tests 
+# curl --location 'http://127.0.0.1:8000/newgrid?gridId=mock1' 
+# curl --location 'http://127.0.0.1:8000/newgrid?gridId=mock2'
+# curl --location 'http://127.0.0.1:8000/newgrid?gridId=mock3' - 404 
