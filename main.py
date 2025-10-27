8f4a00a7f1a2f8be0bc92dd4c5aabcfef57ed57e
Andrew Stepura <andriistepura@gmail.com>
2025-10-27 05:13:26 +0000

    validation endpoint draft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from board_mocks import mock1, mock2
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -37,13 +38,26 @@ def new_grid(gridId:str="mock1"):
     else:
         # https://fastapi.tiangolo.com/tutorial/handling-errors/#use-httpexception
         raise HTTPException(status_code=404, detail=responseErrors)
-    
 
-#TODO add Int tests 
-# curl --location 'http://127.0.0.1:8000/newgrid?gridId=mock1' 
-# curl --location 'http://127.0.0.1:8000/newgrid?gridId=mock2'
-# curl --location 'http://127.0.0.1:8000/newgrid?gridId=mock3' - 404 
-
+#TODO add Int tests  
+# https://fastapi.tiangolo.com/tutorial/testing/#using-testclient
+# curl --location 'http://127.0.0.1:8000/newgrid?gridId=mock1' - 200 and mockId1 with first sudoku body
+# curl --location 'http://127.0.0.1:8000/newgrid?gridId=mock2' - 200 and mockId2 with second sudoku body
+# curl --location 'http://127.0.0.1:8000/newgrid?gridId=mock3' - 404
 
 #TODO for next app versions apply structure with routers
 # https://fastapi.tiangolo.com/tutorial/bigger-applications/#an-example-file-structure
+
+
+
+class GridValidation(BaseModel):
+    gridId: str
+    grid: list
+    errors: list | None = None # No errors expected in req but we want to return them in resp
+
+@app.post("/verifygrid/")
+async def grid_validation(request: GridValidation):    
+    response = request
+    responseErrors = ["fix a1"] #TODO put here  actual grid (game board) validation result
+    response.errors = responseErrors
+    return response
