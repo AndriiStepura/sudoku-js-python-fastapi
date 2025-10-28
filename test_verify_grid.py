3da588e9b99c3f10b218b608e8ce43056ddecf8d
Andrew Stepura <andriistepura@gmail.com>
2025-10-28 19:04:35 +0000

    add tests coverage

--- test_verify_grid.py
+++ test_verify_grid.py
@@ -220,3 +220,58 @@ def test_verify_grid_response_area_duplicate_error():
         verifyGrifResult.errorsMessages
         == "Numbers should not be repeated in region block."
     )
+
+
+def test_verify_grid_response_for_vertical_and_horisontal_duplicates_errors():
+    verifyGrifResult = verify_grid(
+        "mock1",
+        [
+            # Here we simulate change 0 to bad value 8 in x:2,y:6
+            # So expected receive dublication errors with 2:2 and 7:6
+            # and change 0 to good value 5 in x:4,y:4
+            # and change 8 to bad value 6 in x:7,y:6 which should be reverted without dublication errors
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 5, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 8, 0, 0, 0, 2, 6, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9],
+        ],
+    )
+    assert verifyGrifResult.gridId == "mock1"
+    assert verifyGrifResult.grid != []
+    # And expecting that both these values are returned in response
+    assert (verifyGrifResult.grid[6])[2] == 8
+    assert (verifyGrifResult.grid[4])[4] == 5
+    # And owervriting reverted    
+    assert (verifyGrifResult.grid[6])[7] == 8
+    assert verifyGrifResult.grid == [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 5, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 8, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9],
+    ]
+    # So response should back with both wrong cells data for UI
+    assert len(verifyGrifResult.errorCells) == 5
+    assert verifyGrifResult.errorCells == [
+        wrongCell(x=6, y=7, cellErrorMessage="Cheating"),
+        wrongCell(x=2, y=6, cellErrorMessage="Vertical row duplicate"),
+        wrongCell(x=2, y=2, cellErrorMessage="Vertical row duplicate"),
+        wrongCell(x=7, y=6, cellErrorMessage="Horisontal row duplicate"),
+        wrongCell(x=2, y=6, cellErrorMessage="Horisontal row duplicate")
+    ]
+    assert (
+        verifyGrifResult.errorsMessages == [
+            "No cheating! You cannot change the starting grid values.",
+            "Numbers should not be repeated in a vertical row.",
+            "Numbers should not be repeated in a horizontal row."
+        ]        
+    )
